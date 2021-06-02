3240eae2160163230a052f30744dd066a4129f76
Neilsmith <chrisnei@sheridancollege.ca>
2021-06-02 20:03:46 +0200

    Initial Commit

---
+++ src/test/java/sheridan/chrisnei/rockpaperscissors/RockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.chrisnei.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
